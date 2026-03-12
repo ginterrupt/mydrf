b911e81442bbce8d0a90a3add359a5e9531a67ed
ginterrupt <gigalfc@gmail.com>
2026-03-12 18:10:16 +0400

    add users and fields

--- snippets/admin.py
+++ snippets/admin.py
@@ -2,9 +2,13 @@
 
 from django.contrib import admin
 
+
 # Register your models here.
 
 from .models import Snippet
 
 
-admin.site.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ("highlighted",)
+
+admin.site.register(Snippet,SnippetAdmin)
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,4 +1,6 @@
+
 from rest_framework import serializers
+
 
 from .models import Snippet, LANGUAGE_CHOICES,STYLE_CHOICES
 
