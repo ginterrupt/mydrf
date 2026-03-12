94087c5958084ba396cb76e18e642e6d5f6c2fb1
ginterrupt <gigalfc@gmail.com>
2026-03-12 17:12:04 +0400

    add serializes

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+#snippet/view.py
+
+#from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+
+from .models import Snippet
+from .serializers import SnippetSerializer
+
+class SnipetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
